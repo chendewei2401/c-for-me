791ed49852c23514961a195205bb1b686586391c
chendw <chendewei2401@gmail.com>
2020-10-29 20:41:20 +0800

    point sample

--- hello.c
+++ hello.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
 int main(void) {
+
+    int *p = NULL;
+
     printf("hello, %s\n", "world");
+    printf("hello, %d\n", *p);
 }
---
+++ swap.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int * swap(int *px, int *py) {
+    int temp;
+    temp = *px;
+    *px = *py;
+    *py = temp;
+    return px;
+}
+
+int main() {
+    int i = 10, j = 20;
+    int *p = swap(&i, &j);
+    printf("now i=%d, j=%d, *p=%d\n", i, j, *p);
+    return 0;
+}
